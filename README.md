925e254b0c3badf1aef18f53f7f1d4d4b3ce0fcc
Robert Stevahn <rstevahn@pobox.com>
2020-03-06 14:45:27 -0700

    Initial Release
    Project is ready for submission!

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# portfolio-with-node-and-express
- Project 6, Treehouse Full Stack JavaScript Techdegree
+# Portfolio using Node and Express
+### Project 6, Treehouse Full Stack JavaScript Techdegree
+
+Extra Credit features included:
+- package.json has been configured with a start script
+- error handling middleware renders the 'error' Pug template
+- style.css has been customized with changes to some text colors, background colors, font (addition of Merriweather Sans for headlines), and transitions when hovering over the portfolio image links.
+
+This project uses node.js, Express and Pug to serve up a portfolio of each of my JavaScript projects
+to date. It includes an index page, an about page, and dynamically generated project pages for each
+of the projects.
--- app.js
+++ app.js
@@ -4,10 +4,14 @@
 
 /*jshint esversion: 6 */
 
-const express = require('express');
+// Dependencies
+
+const express = require('express'); // Express
 const indexRoute = require('./routes'); // our root router (./routes/index.js)
 const aboutRoute = require('./routes/about'); // our about page router
 const projectsRoute = require('./routes/projects'); // portfolio redirect
+
+// start and configure the server
 
 const app = express(); // start an express server
 
@@ -19,18 +23,24 @@ app.use(indexRoute); // set up our root route
 app.use('/about', aboutRoute); // our about page
 app.use('/projects', projectsRoute); // if they try to go directly to the portfolio
 
+// error handlers
+
 app.use((req, res, next) => {
-    const errNotFound = new Error('Not Found');
-    errNotFound.status = 404;
-    app.render('error', errNotFound);
-    // next(errNotFound);
-  });  
+    const err = new Error('Not Found');
+    err.status = 404;
+    res.locals.error = err;
+    res.status(err.status);
+    console.log('404 Error: Page Not Found');
+    res.render('error');
+});  
 
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
     res.render('error');
-    });
+});
+
+// finally, listen on our port
 
 app.listen(3000, () => {
     console.log('The portfolio application is running on localhost:3000!');
